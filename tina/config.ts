f31314e4cfcf08a151c03c62676023755affcd10
taoguangc <taoguangc@gmail.com>
2025-01-11 19:17:11 +0800

    update tina's mediaroot value to 'uploads'

--- tina/config.ts
+++ tina/config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
     },
     media: {
         tina: {
-            mediaRoot: '',
+            mediaRoot: 'uploads',
             publicFolder: 'public'
         }
     },
